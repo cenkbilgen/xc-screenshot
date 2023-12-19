015ba0e3d91e6d6f21f2bcedb913ea19a67b4b16
cenk <cenk.bilgen@gmail.com>
2023-12-19 15:35:16 -0500

    refactor renaming and ioutil

--- upload_http.go
+++ upload_http.go
@@ -1,38 +1,51 @@
 package main
 
 import (
-  "net/http"
-  "io/ioutil"
-  "log"
-  "regexp"
-  "strconv"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	const port = 7000
- 	uploadHandler := func(w http.ResponseWriter, req *http.Request) {
- 		suggested := suggestedFilename(req.Header)
- 		if suggested == "" {
- 			suggested = "newfile"
- 		}
- 		filename := generateFilename(suggested)
- 		
- 		log.Printf("downloading %v as %v", suggested, filename)
-		body, err := ioutil.ReadAll(req.Body)
+	var port = 7125
+	var err error
+	if len(os.Args) > 1 {
+		port, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalf("specified port not a number")
+		}
+	}
+
+	log.Printf("Listening on port %d\n", port)
+
+	uploadHandler := func(w http.ResponseWriter, req *http.Request) {
+		suggested := suggestedFilename(req.Header)
+		if suggested == "" {
+			suggested = "newfile"
+		}
+		filename := generateAlternateName(suggested)
+
+		log.Printf("downloading %v as %v", suggested, filename)
+		body, err := io.ReadAll(req.Body)
 		if err != nil {
 			log.Printf("error: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 		}
-		err = ioutil.WriteFile(filename, body, 0644)
+		err = os.WriteFile(filename, body, 0644)
 		if err != nil {
 			log.Printf("error: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
-		}	
+		}
 		log.Printf("wrote %v bytes", len(body))
 	}
 
 	http.HandleFunc("/upload", uploadHandler)
-	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(port), nil))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
 
 func suggestedFilename(header http.Header) string {
@@ -45,34 +58,23 @@ func suggestedFilename(header http.Header) string {
 	return filename
 }
 
-func generateFilename(suggested string) string {
-	files, err := ioutil.ReadDir(`.`)
-	if err != nil {
-		log.Fatal(err)
+func generateAlternateName(filename string) string {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return filename
 	}
-	re := regexp.MustCompile(`^` + suggested + `(-(\d+)){0,1}$`)
-	nextn := 0
-	for _, file := range files {
-		matches := re.FindStringSubmatch(file.Name())
-		mnum := len(matches)
-		if mnum == 0 {
-			continue
-		} else if mnum == 1 {
-			if nextn == 0 {
-				nextn = 1
-			}
-		} else if mnum == 3 {
-			filen, _ := strconv.Atoi(matches[2])
-			if filen >= nextn {
-				nextn += 1
-			}
-		} else if mnum == 2 || mnum > 3 {
-			log.Fatal("internal error. check regexp")
+
+	baseName := filename
+	extension := filepath.Ext(filename)
+	if extension != "" {
+		baseName = filename[:len(filename)-len(extension)]
+	}
+
+	i := 1
+	for {
+		alternate := fmt.Sprintf("%s_%d%s", baseName, i, extension)
+		if _, err := os.Stat(alternate); os.IsNotExist(err) {
+			return alternate
 		}
-	}
-	if nextn == 0 {
-		return suggested
-	} else {
-		return suggested + "-" + strconv.Itoa(nextn)
+		i++
 	}
 }
